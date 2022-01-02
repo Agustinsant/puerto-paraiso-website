3126f96570211ef29eb4739a4397ceff8412bbb4
AgustinSant <agustinmsant@gmail.com>
2022-01-02 16:16:41 -0300

    baseURL fixed

--- utils/posadas.js
+++ utils/posadas.js
@@ -1,6 +1,6 @@
 const request = require('postman-request')
 
-const baseURL = 'https://puerto-paraiso-api-v-1.herokuapp.com/'
+const baseURL = 'https://puerto-paraiso-api-v-1.herokuapp.com'
 
 
 const getAllPosadas = (callback) => {
